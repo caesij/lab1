6895ff533ea88c0e0d77296c343ceb5d37422c06
caeia <carllubugin@gmail.com>
2025-09-28 15:55:15 +0800

    Modified line break

--- operators.php
+++ operators.php
@@ -2,26 +2,26 @@
     $a = 10;
     $b = 5;
     
-    echo "a. Arithmetic Operators<br><br>";
+    echo "a. Arithmetic Operators<br /><br />";
 
-    echo "{$a} + {$b} = " . ($a + $b) . "<br>";
-    echo "{$a} - {$b} = " . ($a - $b) . "<br>";
-    echo "{$a} * {$b} = " . ($a * $b) . "<br>";
-    echo "{$a} / {$b} = " . ($a / $b) . "<br>";
-    echo "{$a} % {$b} = " . ($a % $b) . "<br><br><br>";
+    echo "{$a} + {$b} = " . ($a + $b) . "<br />";
+    echo "{$a} - {$b} = " . ($a - $b) . "<br />";
+    echo "{$a} * {$b} = " . ($a * $b) . "<br />";
+    echo "{$a} / {$b} = " . ($a / $b) . "<br />";
+    echo "{$a} % {$b} = " . ($a % $b) . "<br /><br /><br />";
 
-    echo "b. Comparison Operators<br><br>";
+    echo "b. Comparison Operators<br /><br />";
 
-    echo "{$a} == {$b} - ". (($a == $b) ? "true" : "false") . "<br>";
-    echo "{$a} != {$b} - ". (($a != $b) ? "true" : "false") . "<br>";
-    echo "{$a} > {$b} - ". (($a > $b) ? "true" : "false") . "<br>";
-    echo "{$a} < {$b} - ". (($a < $b) ? "true" : "false") . "<br>";
-    echo "{$a} <= {$b} - ". (($a <= $b) ? "true" : "false") . "<br>";
-    echo "{$a} >= {$b} - ". (($a >= $b) ? "true" : "false") . "<br><br><br>";
+    echo "{$a} == {$b} - ". (($a == $b) ? "true" : "false") . "<br />";
+    echo "{$a} != {$b} - ". (($a != $b) ? "true" : "false") . "<br />";
+    echo "{$a} > {$b} - ". (($a > $b) ? "true" : "false") . "<br />";
+    echo "{$a} < {$b} - ". (($a < $b) ? "true" : "false") . "<br />";
+    echo "{$a} <= {$b} - ". (($a <= $b) ? "true" : "false") . "<br />";
+    echo "{$a} >= {$b} - ". (($a >= $b) ? "true" : "false") . "<br /><br /><br />";
 
-    echo "c. Logical Operators<br><br>";
+    echo "c. Logical Operators<br /><br />";
 
-    echo "({$a} == 10 && {$b} == 5) - " . (($a == 10 && $b == 5) ? "true" : "false") . "<br>";
-    echo "({$a} == 10 || {$b} > 9) - " . (($a == 10 || $b > 5) ? "true" : "false") . "<br>";
+    echo "({$a} == 10 && {$b} == 5) - " . (($a == 10 && $b == 5) ? "true" : "false") . "<br />";
+    echo "({$a} == 10 || {$b} > 9) - " . (($a == 10 || $b > 5) ? "true" : "false") . "<br />";
     echo "!({$a} == {$b}) - " . (!($a == $b) ? "true" : "false");
 ?>
